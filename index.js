02b1ca31c53bfbb867ec6525433096c527827e1a
Alex <93370274+Kazimor-5@users.noreply.github.com>
2022-05-27 10:36:38 +0200

    creating client

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// ! IMPORTS
 require('dotenv').config();
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const token = process.env.DISCORD_BOT_TOKEN;
+
+client.once('ready', () => {
+  console.log('Félicitation, votre bot Discord a été correctement initialisé.');
+});
+
+client.login(token);
